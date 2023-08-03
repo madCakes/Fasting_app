211cf710d542e434fbc8484cd98e97fb32c9b2a7
madCakes <jason.pbrooks@hotmail.com>
2023-08-03 13:24:43 +0100

    add icons

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
-
+import { React } from "react";
+import star from "../../assets/star.svg";
+import fire from "../../assets/fire.svg";
+import clock from "../../assets/clock.svg";
 
 const Navbar = () => {
   return (
-    <navbar>Navbar</navbar>
-  )
-}
+    <navbar className="flex  justify-between">
+      {/* Left total days fasted*/}
 
-export default Navbar
+      <div className="flex">
+        <img className="w-10" src={star} />
+        <p>star counter</p>
+      </div>
+      {/* Middle */}
+      <div className="flex">
+        <img className="w-10" src={fire} />
+        <p>daily streak</p>
+      </div>
+      {/* Right */}
+      <div className="flex">
+        <img className="w-10" src={clock} />
+        <p>hours fasted</p>
+      </div>
+    </navbar>
+  );
+};
+
+export default Navbar;

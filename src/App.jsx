db2bdb81eeb964093e8e2c8fc4e0f16e0eeb921d
madCakes <jason.pbrooks@hotmail.com>
2023-08-09 21:05:20 +0100

    add context

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,20 @@
 import React from "react";
-import {Routes, Route} from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 import * as Pages from "./pages";
+import { DataProvider } from "./context";
 
 function App() {
   return (
-    <Routes>
-      <Route index element={<Pages.Login/>}></Route>
-      <Route path="/home" element={<Pages.Home/>}></Route>
-      <Route path="/entry" element={<Pages.Entry/>}></Route>
-      <Route path="*" element={<h1>Uh Oh...</h1>}></Route>
-    </Routes>
+    <DataProvider>
+      <Routes>
+        <Route index element={<Pages.Login />}></Route>
+        <Route path="/home" element={<Pages.Home />}></Route>
+        <Route path="/entry" element={<Pages.Entry />}></Route>
+        <Route path="*" element={<h1>Uh Oh...</h1>}></Route>
+      </Routes>
+    </DataProvider>
   );
 }
 
-export default App
-
+export default App;

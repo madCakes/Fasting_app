eaedd9d14034be352bdc0011ebcbf560e4473817
madCakes <jason.pbrooks@hotmail.com>
2023-08-09 23:49:15 +0100

    add basic elements

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,7 +1,23 @@
 import React from "react";
+import star from "../../assets/star.svg";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <navbar className="flex justify-around p-1 pt-2 mx-4 pb-2 absolute inset-x-0 bottom-0">
+      <div className="flex flex-col">
+        <img className="w-7 mx-auto my-1" src={star} alt="Today" />
+        <p>Today</p>
+      </div>
+      <div className="flex flex-col">
+        <img className="w-7 mx-auto my-1" src={star} alt="Fast" />
+        <p>Fast</p>
+      </div>
+      <div className="flex flex-col">
+        <img className="w-7 mx-auto my-1" src={star} alt="Profile" />
+        <p>Profile</p>
+      </div>
+    </navbar>
+  );
 };
 
 export default Footer;

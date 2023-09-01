04069628f7d252e73d149bede443104ed35a9e4e
madCakes <jason.pbrooks@hotmail.com>
2023-09-01 18:38:32 +0100

    setup piechart

--- src/components/Main/index.jsx
+++ src/components/Main/index.jsx
@@ -1,6 +1,12 @@
 import { React, useState } from "react";
+import Chart from "chart.js/auto";
+import { CategoryScale } from "chart.js";
+import { Data } from "../../utils/Data"
+import PieChart from "../PieChart";
 
 import { WeightForm } from "../";
+
+Chart.register(CategoryScale);
 
 const Main = () => {
   const [water, setWater] = useState(0);
@@ -12,11 +18,30 @@ const Main = () => {
     setIsOpen((isOpen) => !isOpen);
   };
 
+  const [chartData, setChartData] = useState({
+    labels: Data.map((data) => data.year), 
+    datasets: [
+      {
+        label: "Users Gained ",
+        data: Data.map((data) => data.userGain),
+        backgroundColor: [
+          "rgba(75,192,192,1)",
+          "rgb(5 150 105)"
+        ],
+        borderColor: "none",
+        borderWidth: 0
+      }
+  ]
+  });
+
+
   const timeRemaining = "00:00:00"
   return (
     <section className="flex flex-col mt-2 mx-5">
       <div className="flex p-2 border-solid border-2 border-slate-900 rounded">
-        <div className="my-auto mx-10">Fasting chart</div>
+        <div className="my-auto mx-10">
+        <PieChart chartData={chartData} />
+        </div>
         <div className="m-1">
           <p className="text-xl">{timeRemaining}</p>
           <h1>fasting time remaining </h1>
